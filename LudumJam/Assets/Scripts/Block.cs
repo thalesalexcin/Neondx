944178db8f0aebaab12d87fd6debd3f22080b1f8
thalesalexcin@gmail.com <thalesalexcin@gmail.com>
2016-04-16 15:06:24 +0200

    Validation

---
+++ LudumJam/Assets/Scripts/Block.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour 
+{
+    public Color AliveColor;
+    public Color DestroyedColor;
+
+    public bool Destroyed;
+    private SpriteRenderer _SpriteRenderer;
+
+    void OnValidate()
+    {
+        _SpriteRenderer = GetComponent<SpriteRenderer>();
+        _UpdateSprite();
+    }
+
+    void Awake()
+    {
+        _SpriteRenderer = GetComponent<SpriteRenderer>();
+        _UpdateSprite();
+    }
+
+    public void Toggle()
+    {
+        Destroyed = !Destroyed;
+        _UpdateSprite();        
+    }
+
+    private void _UpdateSprite()
+    {
+        if (Destroyed)
+            _Destroy();
+        else
+            _Revive();
+    }
+
+    private void _Destroy()
+    {
+        _SpriteRenderer.color = DestroyedColor;   
+    }
+
+    private void _Revive()
+    {
+        _SpriteRenderer.color = AliveColor;
+    }
+}
---
+++ LudumJam/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+    enum GameState
+    {
+        PLAYING,
+        
+    }
+
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    private GameObject _GetNextCursor()
+    {
+        return null;
+    }
+
+    private GameObject _GetNextObject()
+    {
+        return null;
+    }
+}
